25607dc5de23e5c405250eb7e33936c29f87dde3
Harish <harish.cse2027@gmail.com>
2026-06-22 23:55:06 +0530

    Add to Week-1 FinancialForecast Exercise

---
+++ Week-1/DS-7-FinancialForecast/FinancialForecast.java
@@ -0,0 +1,20 @@
+public class FinancialForecast {
+
+    // Recursive method
+    public static double predictFutureValue(double currentValue,
+                                            double growthRate,
+                                            int years) {
+
+        // Base Case
+        if (years == 0) {
+            return currentValue;
+        }
+
+        // Recursive Case
+        return predictFutureValue(
+                currentValue,
+                growthRate,
+                years - 1
+        ) * (1 + growthRate);
+    }
+}
